b30ba1d3209c2da296cfc889334dd4be8108bbb2
githubsamtest <66143359+githubsamtest@users.noreply.github.com>
2020-09-29 15:04:26 -0700

    Create qac200916_300.c

---
+++ qac200916_300.c
@@ -0,0 +1,308 @@
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+vDnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+vvDnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+vDnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+vDnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+vDnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
