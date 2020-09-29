6fdf22e1b9b438643d55bd734c75451d9dc2e17f
githubsamtest <66143359+githubsamtest@users.noreply.github.com>
2020-09-29 15:40:52 -0700

    Create 1200.py

---
+++ 1200.py
@@ -0,0 +1,1232 @@
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+vDnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+vvDnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+vDnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+vDnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+vDnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+vDnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+vvDnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+vDnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+vDnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+vDnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+vDnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+vvDnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+vDnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+vDnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+vDnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+vDnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+vDnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+vvDnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+vDnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+vDnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+vDnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
+
+Dnepr
+Samara
+New york
+Moscow
